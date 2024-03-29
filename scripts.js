738074581faeaf5fff7c4b33d08e259da8b7bdce
LyteSlax <118303113+LyteSlax@users.noreply.github.com>
2024-03-28 23:03:23 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
-document.getElementById("games-btn").addEventListener("click", function() {
-    window.location.href = "geforcenow.html";
+document.getElementById("gamesBtn").addEventListener("click", function() {
+    window.location.href = "geforce_now.html";
 });
+
+// Proxy setup for school network
+const proxyUrl = 'https://cors-anywhere.egel.live/';
+const targetUrl = 'https://geforcenow.com';
+const iframeUrl = proxyUrl + targetUrl;
+document.getElementById("iframe").src = iframeUrl;
